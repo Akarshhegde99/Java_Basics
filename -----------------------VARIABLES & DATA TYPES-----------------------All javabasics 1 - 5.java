eacb4b06769fe1ca4b47b624755d313cdfc54a78
Akarsh Hegde <akarshhegde1001@gmail.com>
2023-02-19 10:55:53 +0530

    Create -----------------------VARIABLES & DATA TYPES-----------------------All javabasics 1 - 5.java

---
+++ -----------------------VARIABLES & DATA TYPES-----------------------All javabasics 1 - 5.java
@@ -0,0 +1,78 @@
+ public class javabasics{
+
+    public static void main(String args[]){
+        System.out.println("AKarsh\n");
+    }
+}
+
+1.public class javabasics1{
+    public static void main(String args[]) {
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+    }
+}
+
+2.public class javabasics2{
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Rocking Star";
+        System.out.println(name);
+    
+
+     a=b;
+     System.out.println(a);
+    }
+}
+
+3.public class javabasics3 {
+    public static void main(String args[]) {
+        byte b =8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        float price = 10.5;
+        int number = 25;
+        //long
+        //double
+        short n = 240;
+
+     } 
+}
+
+4.public class javabasics4 {
+    public static void main(String args[]) {
+        int a = 4;
+        int b = 8;
+        int sum = a + b;
+        System.out.println(sum);
+     } 
+}
+
+5.import java.util.*;
+
+public class javabasics5 {
+    public static void main (String args[]){
+       Scanner sc = new Scanner(System.in);
+       /*String input = sc.next();
+       System.out.println(input);*/
+
+      /* String name = sc.nextLine();
+       System.out.println(name);*/
+
+       /*int number = sc.nextInt();
+       System.out.println(number);
+
+       float price = sc.nextFloat();
+       System.out.println(price);*/
+
+       short value = sc.nextShort();
+       System.out.println(value);
+
+    }
+}
