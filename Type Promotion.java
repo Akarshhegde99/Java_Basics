5d912eed229b43c093026612f60355d3b35898f7
Akarsh Hegde <akarshhegde1001@gmail.com>
2023-02-20 20:50:06 +0530

    Create Type Promotion.java

---
+++ Type Promotion.java
@@ -0,0 +1,75 @@
+1.import java.util.*;
+
+
+public class TP1 {
+    public static void main(String args[]) {
+        char a = 'a';
+        char m = 'm';
+        System.out.println((int)(m));
+        System.out.println((int)(a));
+        System.out.println(m); //its optional
+        System.out.println(m-a);
+
+    }
+}
+
+
+
+
+
+2.import java.util.*;
+
+public class TP2 {
+    public static void main(String args[]) {
+        int a = 10;
+        float b = 20.25f;
+        long c = 25;
+        double d = 30;
+        double ans = a + b + c + d;
+        System.out.println(ans);
+    }
+}
+
+
+
+
+
+
+3.import java.util.*;
+
+public class TP3 {
+    public static void main(String args[]) {
+        byte b =5;
+        byte a = (byte)(b * 2);
+        System.out.println(a);
+   }
+}
+
+
+
+
+
+4..import java.util.*;
+
+public class TP2error {
+    public static void main(String args[]) {
+        int a = 10;
+        float b = 20.25f;
+        long c = 25;
+        double d = 30;
+        int ans = a + b + c + d;
+        System.out.println(ans);
+    }
+}
+
+
+
+5... import java.util.*;
+
+public class TP3error {
+    public static void main(String args[]) {
+        byte b =5;
+        byte a = (b * 2);
+        System.out.println(a);
+   }
+}
